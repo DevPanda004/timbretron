38cb2054534a2557b3893c3c8aaa9e3dbd3e8f59
DevPanda004 <dipanshu.panda75@gmail.com>
2024-10-12 17:18:32 +0530

    Update urmp.py

--- data_prep/urmp.py
+++ data_prep/urmp.py
@@ -22,9 +22,28 @@ def urmp_prep_wavs(outdir, instrument_files, src):
         
 trumpet_files = get_audiosep_ins('tpt')
 violin_files = get_audiosep_ins('vn')
-
+flute_files = get_audiosep_ins('fl')
+cello_files = get_audiosep_ins('vc')
+trombone_files = get_audiosep_ins('tbn')
+viola_files = get_audiosep_ins('va')
+doublebass_files = get_audiosep_ins('db')
+oboe_files = get_audiosep_ins('ob')
+bassoon_files = get_audiosep_ins('bn')
+saxophone_files = get_audiosep_ins('sax')
+horn_files = get_audiosep_ins('hn')
+tuba_files = get_audiosep_ins('tba')
+clarinet_files = get_audiosep_ins('cl')
 # Data preparation
 urmp_prep_wavs(args.outdir+'spkr_1', trumpet_files, 'tpt')
 urmp_prep_wavs(args.outdir+'spkr_2', violin_files, 'vn')
-#urmp_prep_wavs(args.outdir+'spkr_3', flute_files, 'fl')  # uncomment to include flute
-#urmp_prep_wavs(args.outdir+'spkr_4', cello_files, 'vc')  # uncomment to include cello
+urmp_prep_wavs(args.outdir+'spkr_3', flute_files, 'fl')  
+urmp_prep_wavs(args.outdir+'spkr_4', cello_files, 'vc')
+urmp_prep_wavs(args.outdir+'spkr_5', trombone_files, 'tbn')
+urmp_prep_wavs(args.outdir+'spkr_6', viola_files, 'va')
+urmp_prep_wavs(args.outdir+'spkr_7', doublebass_files, 'db')
+urmp_prep_wavs(args.outdir+'spkr_8', oboe_files, 'ob')
+urmp_prep_wavs(args.outdir+'spkr_9', bassoon_files, 'bn')
+urmp_prep_wavs(args.outdir+'spkr_10', saxophone_files, 'sax')
+urmp_prep_wavs(args.outdir+'spkr_11', horn_files, 'hn')
+urmp_prep_wavs(args.outdir+'spkr_12', tuba_files, 'tb')
+urmp_prep_wavs(args.outdir+'spkr_13', clarinet_files, 'cl')  
